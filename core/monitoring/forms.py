a5072bd73720048a2466f8a38b711992bef8d4bd
Luccascarpa <ms-lucca1@hotmail.com>
2025-01-27 01:10:12 -0300

    add coments on forms classes

--- core/monitoring/forms.py
+++ core/monitoring/forms.py
@@ -2,11 +2,13 @@ from django import forms
 from .models import Asset
 
 class AssetForm(forms.ModelForm):
+    # Form related to Asset model, used to register/edit Assets
     class Meta:
         model = Asset
         fields = ["name", "symbol", "inferior_limit", "superior_limit", "frequency"]
 
 
-class PriceHistoryForm(forms.Form):
+class PriceHistoryFilterForm(forms.Form):
+    # Form for filtering price history data
     start_date = forms.DateTimeField(required=False, label = "Start date")
     end_date = forms.DateTimeField(required=False, label = "End date")
