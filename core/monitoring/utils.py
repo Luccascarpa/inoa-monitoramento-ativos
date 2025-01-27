e4088043f7ff3b07ddedab912fea2361f026ba65
Luccascarpa <ms-lucca1@hotmail.com>
2025-01-27 01:19:16 -0300

    Create utils file for useful functions on the app

---
+++ core/monitoring/utils.py
@@ -0,0 +1,18 @@
+import yfinance as yf
+from decimal import Decimal
+
+def fetch_asset_current_price(symbol: str) -> Decimal:
+    # Getting asset's current price using yahoo finance's API
+    try:
+        # getting data througn the symbole and it's current price
+        asset = yf.Ticker(symbol)
+        price = asset.info.get('regularMarketPrice')
+
+        if not price:
+            raise ValueError(f"Can't obtain {symbol}'s price. Check if the symbol is correct")
+
+        return Decimal(price)
+    
+    except Exception as e:
+        print(f"Something went wrong searching the asset {symbol}: {e}")
+        return Decimal("0.0")
