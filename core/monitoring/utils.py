fdaa9cc75a607bc5a5ac0b0490e8408c0009a5b8
Luccascarpa <ms-lucca1@hotmail.com>
2025-01-28 19:35:23 -0300

    create function to check and save price, and warn if it crosses limits

--- core/monitoring/utils.py
+++ core/monitoring/utils.py
@@ -1,6 +1,8 @@
 import yfinance as yf
 from decimal import Decimal
 
+from models import Asset, PriceHistory
+
 def fetch_asset_current_price(symbol: str) -> Decimal:
     # Getting asset's current price using yahoo finance's API
     try:
@@ -16,3 +18,20 @@ def fetch_asset_current_price(symbol: str) -> Decimal:
     except Exception as e:
         print(f"Something went wrong searching the asset {symbol}: {e}")
         return Decimal("0.0")
+    
+def send_alert_email(asset, price, type):
+    pass
+    
+def check_and_save_price(asset: Asset):
+    # Checks and saves assets current price, and sends email in case it crosses limits
+
+    price = fetch_asset_current_price(asset.symbol)
+    PriceHistory.objects.create(asset, price)
+
+    # If price cross lower limt, sugest buying
+    if price <= asset.inferior_limit:
+        send_alert_email(asset, price, type='buy')
+
+    # If price cross upper limit, sugest selling
+    elif price >= asset.superior_limit:
+        send_alert_email(asset, price, type='sell')
