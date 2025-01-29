09740cc26887735f350df9de34dd69d3d9e6fe84
Luccascarpa <ms-lucca1@hotmail.com>
2025-01-28 21:16:47 -0300

    create task for cehcking assets prices

---
+++ core/monitoring/tasks.py
@@ -0,0 +1,12 @@
+# using celery as scheduler
+
+from celery import shared_task
+from models import Asset
+from utils import check_and_save_price
+
+
+
+@shared_task
+def check_prices_periodically():
+    # Task for checking the price of all assets
+    for asset in Asset.objects.all(): check_and_save_price(asset)
