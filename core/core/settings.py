b5f4b3a05a9a256cb86259858482e8938ba6499a
Luccascarpa <ms-lucca1@hotmail.com>
2025-01-28 21:27:11 -0300

    add celery

--- core/core/settings.py
+++ core/core/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'monitoring'
+    'monitoring',
+    'django_celery_beat'
 ]
 
 MIDDLEWARE = [
@@ -137,3 +138,8 @@ EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+
+# Celery
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
