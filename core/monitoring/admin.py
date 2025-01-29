93d438479189006fcbbf43b1b33f37556e71ed88
Luccascarpa <ms-lucca1@hotmail.com>
2025-01-29 17:31:23 -0300

    Define models in admin

--- core/monitoring/admin.py
+++ core/monitoring/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Asset, PriceHistory
+
+#admin.site.register(Asset)
+#admin.site.register(PriceHistory)
+
+from django.contrib import admin
+from .models import Asset, PriceHistory
+
+@admin.register(Asset)
+class AssetAdmin(admin.ModelAdmin):
+    list_display = ('symbol', 'name', 'inferior_limit', 'superior_limit', 'frequency')
+
+@admin.register(PriceHistory)
+class PriceHistoryAdmin(admin.ModelAdmin):
+    list_display = ('asset', 'price', 'timestamp')
